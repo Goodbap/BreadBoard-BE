a2159128c78e6571ae24201101b0aff40cbfb200
kymin <park48664866@gmail.com>
2024-01-21 18:47:53 +0900

    initial commit

---
+++ src/main/java/com/goodbap/breadboard/BreadboardApplication.java
@@ -0,0 +1,13 @@
+package com.goodbap.breadboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreadboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreadboardApplication.class, args);
+	}
+
+}
