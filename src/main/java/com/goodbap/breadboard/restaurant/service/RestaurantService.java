4ccd90a4c70f800a57abf2d5a06f9962b6fa0f9c
kymin <park48664866@gmail.com>
2024-01-30 15:48:41 +0900

    feat: 예외처리 추가
    예외처리 추가:
    - 카카오api 호출 시 예외처리 추가

--- src/main/java/com/goodbap/breadboard/restaurant/service/RestaurantService.java
+++ src/main/java/com/goodbap/breadboard/restaurant/service/RestaurantService.java
@@ -2,10 +2,11 @@ package com.goodbap.breadboard.restaurant.service;
 
 import java.util.Map;
 
-import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
-
 import com.goodbap.breadboard.restaurant.dto.RequestDto;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.ErrorResponseException;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 public class RestaurantService {
@@ -16,16 +17,25 @@ public class RestaurantService {
     }
 
     public Map<String, Object> getRestaurants(RequestDto dto) {
-        Map<String, Object> response = webClient.get()
-                .uri(uriBuilder -> uriBuilder
-                        .queryParam("category_group_code", "FD6")
-                        .queryParam("y", dto.getLatitude())
-                        .queryParam("x", dto.getLongitude())
-                        .queryParam("radius", dto.getRadius())
-                        .build())
-                .retrieve()
-                .bodyToMono(Map.class)
-                .block();   //예외처리 필요
-        return response;
+        try {
+            Map<String, Object> response = webClient.get()
+                    .uri(uriBuilder -> uriBuilder
+                            .queryParam("category_group_code", "FD6")
+                            .queryParam("y", dto.getLatitude())
+                            .queryParam("x", dto.getLongitude())
+                            .queryParam("radius", dto.getRadius())
+                            .build())
+                    .retrieve()
+                    .bodyToMono(Map.class)
+                    .block();
+            return response;
+        } catch (ErrorResponseException e) {
+            // System.out.println(e.getMessage());
+            throw e;
+        }
+        /*
+         * 응답결과
+         * {documents : list<object<식당정보>>, meta : object}
+         */
     }
 }
