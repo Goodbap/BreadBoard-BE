36177e0b32e1efaf0064b8af16449339281f1c75
kymin <park48664866@gmail.com>
2024-02-18 16:52:09 +0900

    test: 기능 구현에 따른 테스트코드 수정

--- src/test/java/com/goodbap/breadboard/restaurant/service/RestaurantServiceTest.java
+++ src/test/java/com/goodbap/breadboard/restaurant/service/RestaurantServiceTest.java
@@ -1,26 +1,27 @@
 package com.goodbap.breadboard.restaurant.service;
 
-import java.util.Map;
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.goodbap.breadboard.restaurant.domain.Restaurant;
 import com.goodbap.breadboard.restaurant.dto.RequestDto;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-import static org.assertj.core.api.Assertions.assertThat;
 
 public class RestaurantServiceTest {
     @Test
     public void getRestaurantsTest() {
         // given
         RequestDto dto = mock(RequestDto.class);
-        Map<String, Object> response = new HashMap<String, Object>();
+        List<Restaurant> response = new ArrayList<Restaurant>();
         RestaurantService service = mock(RestaurantService.class);  // mock객체(모의 객체) 생성
         when(service.getRestaurants(dto)).thenReturn(response); // 가짜 반환값 설정(stubbing)
         // when
-        Map<String, Object> result = service.getRestaurants(dto);
+        List<Restaurant> result = service.getRestaurants(dto);
         // then
         assertThat(result).isEqualTo(response);
     }
